7359f4a07b9ebea17ab331793a00b9c28065e4fd
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-17 17:00:21 -0300

    Estlização da Seção texto

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -15,7 +15,7 @@ export default function LandingPage () {
 
             </section>
 
-            <section className='secao-texto'>
+            <section className='limitar-secao secao-texto'>
                 <h1>Bem-vindo a Barber Shop</h1>
                 <p>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
                 <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
